685e0c3ee7f5b11f09c5c0f13ac217ff31cc25d2
jaewong1994 <jaewong1994@gmail.com>
2026-09-03 17:34:16 +0900

    Enable Supabase realtime claims

--- realtime-config.js
+++ realtime-config.js
@@ -1,6 +1,5 @@
 window.PA_REALTIME_CONFIG = {
-  enabled: false,
-  url: "",
-  publishableKey: "",
+  enabled: true,
+  url: "https://reyydhlreabavrbmomkz.supabase.co",
+  publishableKey: "sb_publishable_j4jW3T8qT-cvSN5m7rmFEQ_WyF3TtSP",
 };
-
